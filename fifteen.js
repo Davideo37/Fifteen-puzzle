a1460d858ab5b7d480f280b8d598780ba26a56c2
Davideo37 <damargur@gmail.com>
2021-09-21 19:48:57 -0400

    Refactored some code

--- fifteen.js
+++ fifteen.js
@@ -3,24 +3,27 @@ class grid {
     this.id = id;
     this.id.appendChild(document.createElement("div"));
     this.tiles = id.getElementsByTagName("div");
-    for (let i = 0; i < this.tiles.length; i++) {
-      this.tiles[i].id = "tile" + i.toString();
-      this.tiles[i].classList.add("active");
-      this.tiles[i].addEventListener('click', function () { makeMove("tile" + i.toString()) });
+    this.tileNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let count = 0;
+    for (let tile of this.tiles) {
+      let tileId = "tile" + count.toString();
+      tile.id = tileId;
+      tile.classList.add("active");
+      tile.addEventListener('click', () => this.makeMove(tileId));
+      count++;
     }
   }
+  makeMove = function(id) {
+    let tempTile = document.getElementById(id);
+    let emptyTile = document.getElementById("tile15");
+    document.getElementById("tile15").innerHTML = tempTile.innerHTML;
+    document.getElementById("tile15").id = tempTile.id;
+    document.getElementById(id).innerHTML = "";
+    document.getElementById(id).id = "tile15";
+    
+  }
+}
 
-  
-}
-function makeMove(id) {
-  let tempTileIH = document.getElementById(id).innerHTML;
-  document.getElementById("tile15").innerHTML = tempTileIH;
-  document.getElementById("tile15").id = id;
-  document.getElementById(id).innerHTML = "";
-  document.getElementById(id).id = "tile15";
-  
-  
-}
 
 window.onload = () => {
   new grid(document.getElementById("puzzlearea"));
