38873c3286a79c794df581d0b23dc200411543ae
Davideo37 <damargur@gmail.com>
2021-09-22 22:22:26 -0400

    Fully functional shuffle

--- fifteen.js
+++ fifteen.js
@@ -8,39 +8,48 @@ class grid {
     for (let tile of this.tiles) {
       let tileId = "T" + count.toString();
       tile.id = tileId;
-      tile.onclick = function () { update(tileId) };
+      tile.onclick = function () {
+        update(tileId);
+      };
       count++;
     }
   }
 
   /** Function to update and refresh the board when a tile is clicked
-   * 
+   *
    * @param {string} id - the id of the tile clicked on
    */
-  updateBoard = function(id) {
-    if (this.isBlankNeighbor(id)) { // Check if tile clicked on is next to the blank space and swap
+  updateBoard = function (id) {
+    if (this.isBlankNeighbor(id)) {
+      // Check if tile clicked on is next to the blank space and swap
       this.swapTile(id);
     }
     let count = 0;
-    for (let tile of this.tiles) { // Now go through and set all ids to match the array
+    for (let tile of this.tiles) {
+      // Now go through and set all ids to match the array
       tile.id = "T" + this.tileNums[count].toString();
       tile.innerHTML = this.tileNums[count].toString();
-      if (tile.id == "T16") { tile.innerHTML = "";}
-      tile.onclick = function () { update(tile.id); };
-      if (this.isBlankNeighbor(tile.id)) { // If a tile is now neighboring the blank tile, make it hoverable
+      if (tile.id == "T16") {
+        tile.innerHTML = "";
+      }
+      tile.onclick = function () {
+        update(tile.id);
+      };
+      if (this.isBlankNeighbor(tile.id)) {
+        // If a tile is now neighboring the blank tile, make it hoverable
         tile.classList.add("hoverable");
       } else {
         tile.classList.remove("hoverable");
       }
       count++;
     }
-  }
-  
-  /** Function to swap a tile with the blank tile "T16". 
-   * 
+  };
+
+  /** Function to swap a tile with the blank tile "T16".
+   *
    *  It will search the array of tileNums, find where both tiles are, and swap their positions
-   * 
-   * @param {string} id 
+   *
+   * @param {string} id
    */
   swapTile = function (id) {
     let tilei = this.findTile(id);
@@ -48,27 +57,30 @@ class grid {
     let temp = this.tileNums[tilei];
     this.tileNums[tilei] = this.tileNums[blanki];
     this.tileNums[blanki] = temp;
-  }
-
+  };
 
   /** Check if a tile is adjacent to the blank tile, T16
-   * 
-   * @param {string} id 
+   *
+   * @param {string} id
    * @returns true if the tile is neighboring, false otherwise
    */
   isBlankNeighbor = function (id) {
     let tilei = this.findTile(id);
-    if (this.tileNums[tilei + 1] == 16 || this.tileNums[tilei - 1] == 16 ||
-      this.tileNums[tilei + 4] == 16 || this.tileNums[tilei - 4] == 16) {
+    if (
+      this.tileNums[tilei + 1] == 16 ||
+      this.tileNums[tilei - 1] == 16 ||
+      this.tileNums[tilei + 4] == 16 ||
+      this.tileNums[tilei - 4] == 16
+    ) {
       return true;
     }
     return false;
-  }
+  };
 
   /** Searches the list of tileNums to find the position of the tile inputted
-   * 
-   * @param {string} id 
-   * @returns 
+   *
+   * @param {string} id
+   * @returns
    */
   findTile(id) {
     for (let i = 0; i < this.tileNums.length; i++) {
@@ -78,6 +90,17 @@ class grid {
     }
     return -1;
   }
+
+  async shuffle(amount) {
+    for (let i = 0; i < amount; i++) {
+      let possibleMoves = document.getElementsByClassName("hoverable");
+      this.updateBoard(possibleMoves[Math.floor(Math.random() * possibleMoves.length)].id);
+      await this.sleep(10);
+    }
+  }
+  sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 }
 
 
@@ -91,8 +114,11 @@ function update(id) {
   board.updateBoard(id);
 }
 
+
 let board;
 window.onload = () => {
+  let button = document.getElementById("shufflebutton");
   board = new grid(document.getElementById("puzzlearea"));
   board.updateBoard("0");
+  button.onclick = function () { board.shuffle(1000) };
 };
